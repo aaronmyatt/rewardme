1e8f235450acbdac746c63b83691da2113f7d194
atm <aaronmyatt@gmail.com>
2022-02-14 20:06:54 +0800

    utility to preview data in dev mode

---
+++ src/components/DevJson.vue
@@ -0,0 +1,33 @@
+<template>
+  <q-expansion-item
+    v-if="IS_DEV"
+    icon="settings"
+    label="Data"
+  >
+    <q-card>
+      <q-card-section>
+        <pre>{{ devjson }}</pre>
+      </q-card-section>
+    </q-card>
+  </q-expansion-item>
+</template>
+<script lang="ts">
+export default {
+  props: ['jsonable'],
+  data() {
+    return {
+      IS_DEV: import.meta.env.DEV,
+    }
+  },
+  computed: {
+    devjson(): string {
+      try {
+        return JSON.stringify(this.jsonable, null, '  ')
+      }
+      catch (e) {
+        return e.message
+      }
+    },
+  },
+}
+</script>
