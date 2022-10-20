65d706778aba380c17baab87f4bd8a1f244ca520
aaronmyatt <aaronmyatt@gmail.com>
2022-10-20 21:51:12 +0800

    add additional point change options

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <!--suppress TypeScriptCheckImport -->
 <script setup lang="ts">
 import PubSub from 'pubsub-js'
+import DevJson from '~/components/DevJson.vue'
 import useActiveProfileImage from '~/composables/useActiveProfileImage'
 import useProfiles from '~/composables/useProfiles'
 import useRewards from '~/composables/useRewards'
@@ -20,6 +21,8 @@ onMounted(() => {
   Object.assign(profile, getActiveProfile())
 })
 const { profileImage } = useActiveProfileImage()
+const openEditPointsDialog = ref(false)
+const editPointsAmount = ref(0)
 
 </script>
 
@@ -38,36 +41,87 @@ const { profileImage } = useActiveProfileImage()
           <q-img v-if="profileImage" :src="profileImage" class="rounded-full" />
           <DefaultAvatar v-else class="w-full h-auto rounded-full bg-white" />
         </div>
-        <div class="col-9 pl-4 column justify-center">
-          <p class="text-xl font-bold text-primary">
+        <div class="col-9 column flex-nowrap">
+          <p class="row text-4xl font-extrabold text-[#4F5D90] leading-loose capitalize">
             {{ profile && profile.name }}
           </p>
-          <p class="text-lg font-bold text-red-800 opacity-50">
-            {{ count }} ({{ totalClaimed }})
-          </p>
+          <div class="row items-center justify-center h-full text-center">
+            <div class="col-6">
+              <p class="text-xl font-bold text-[#4F5D90]">
+                {{ count }}
+              </p>
+              <p class="text-xl text-[#4F5D90]">
+                Total Points
+              </p>
+            </div>
+            <div class="col-6">
+              <p class="text-xl font-bold text-[#4F5D90]">
+                {{ totalClaimed }}
+              </p>
+              <p class="text-xl text-[#4F5D90]">
+                Claimed
+              </p>
+            </div>
+          </div>
         </div>
       </q-card-section>
       <q-separator />
       <q-card-section class="row p-0">
         <q-btn
           flat
-          class="col-6 aspect-video"
+          class="col-6 aspect-video py-6"
           @click="PubSub.publish(Topics.DISCOUNT_BEHAVIOUR, {id: profile.id})"
         >
           <Subtract class="text-4xl bg-primary rounded-full text-white" />
         </q-btn>
         <q-btn
           flat
-          class="col-6 aspect-video"
+          class="col-6 aspect-video py-6"
           @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id})"
         >
           <Add class="text-4xl bg-primary rounded-full text-white" />
         </q-btn>
       </q-card-section>
+      <q-card-section class="p-0">
+        <q-btn-group spread outline class="row space-x-px">
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="Edit Points" @click="openEditPointsDialog = !openEditPointsDialog" />
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="5" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: 5})" />
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="10" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: 10})" />
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="20" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: 20})" />
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="50" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: 50})" />
+          <q-btn class="bg-primary text-secondary font-bold py-4" no-caps label="100" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: 100})" />
+        </q-btn-group>
+      </q-card-section>
+      <q-separator />
       <q-card-section class="row p-0">
-        <q-btn to="/rewards" class="bg-primary text-secondary font-bold col-12 rounded-b-xl py-4" no-caps flat label="Redeem Points" />
+        <q-btn to="/rewards" class="bg-primary text-secondary font-bold col-12 rounded-b-xl py-4" no-caps flat label="Claim Rewards" />
       </q-card-section>
     </q-card>
+
+    <q-dialog v-model="openEditPointsDialog">
+      <DevJson :jsonable="editPointsAmount" />
+
+      <q-card style="min-width: 350px">
+        <q-card-section>
+          <div class="text-h6">
+            Edit Points
+          </div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <q-input v-model.number="editPointsAmount" type="number" dense autofocus />
+        </q-card-section>
+
+        <q-card-actions class="text-primary row justify-around">
+          <q-btn v-close-popup flat class="col-5" @click="PubSub.publish(Topics.DISCOUNT_BEHAVIOUR, {id: profile.id, change: editPointsAmount})">
+            <Subtract class="text-4xl bg-primary rounded-full text-white" />
+          </q-btn>
+          <q-btn v-close-popup flat class="col-5" @click="PubSub.publish(Topics.REWARD_BEHAVIOUR, {id: profile.id, change: editPointsAmount})">
+            <Add class="text-4xl bg-primary rounded-full text-white" />
+          </q-btn>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
     <div class="dashboardcontainer pt-4">
       <div class="profiles flex bg-secondary">
