2c90e0406d60b5b4071616f4715309f157e75d79
aaronmyatt <aaronmyatt@gmail.com>
2022-10-23 16:00:46 +0800

    use one column on smallest devices

--- src/components/RewardList.vue
+++ src/components/RewardList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-primary grid grid-cols-2 md:grid-cols-3 gap-3">
+  <div class="text-primary grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
     <RewardCard v-for="reward in sortedRewards" :key="reward.id" :reward="reward" :reward-image="reward.image && rewardImages[reward.image]" class="w-full" />
   </div>
 </template>
