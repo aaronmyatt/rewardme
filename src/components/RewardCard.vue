a61f17caa76718064a9f43a561bbbe60425b0522
aaronmyatt <aaronmyatt@gmail.com>
2022-11-02 14:44:53 +0800

    allow reward cards to be laid out horizontally

--- src/components/RewardCard.vue
+++ src/components/RewardCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import imageCache from '~/composites/imageCache'
 import type { IReward } from '~/schemas'
 import useRewards from '~/composables/useRewards'
 
@@ -6,12 +7,22 @@ const { claimReward, availablePoints, claimable } = useRewards()
 
 interface Props {
   reward: IReward
-  rewardImage?: string
   editable?: boolean
+  horizontal?: boolean
+  showProgress?: boolean
 }
 
 const props = withDefaults(defineProps<Props>(), {
   editable: true,
+  showProgress: true,
+})
+const rewardImage = ref('')
+
+watchEffect(async() => {
+  if (props.reward.image !== undefined) {
+    const image = await imageCache.getImage(props.reward.image)
+    if (image) rewardImage.value = image
+  }
 })
 
 const untilClaimable = computed(() => {
@@ -23,42 +34,43 @@ const untilClaimable = computed(() => {
 
 <template>
   <q-card class="rounded-xl">
-    <q-card-section class="p-0">
-      <q-img
-        class="rounded-t-xl"
-        :ratio="16/9"
-        :src="rewardImage"
-        spinner-color="primary"
-        spinner-size="82px"
-      >
-        <div class="text-2xl absolute bottom-0 left-0 text-left capitalize reward-name-container">
+    <q-card-section :horizontal="horizontal" class="p-0">
+      <q-card-section class="col-4" :class="{'p-0': !horizontal}">
+        <q-img
+          class="rounded-xl"
+          :ratio="1"
+          :src="rewardImage"
+          spinner-color="primary"
+          spinner-size="82px"
+        >
+          <div v-if="!horizontal" class="text-base md:text-xl absolute bottom-0 left-0 text-left capitalize reward-name-container">
+            <div class="space-x-4 flex">
+              <div class="name">
+                {{ reward.name }}
+              </div>
+              <carbon-star v-if="reward.claimed" class="claimed text-yellow-400 fill-yellow-400" />
+            </div>
+            <div v-if="showProgress" class="text-base md:text-xl">
+              {{ untilClaimable }}
+            </div>
+          </div>
+        </q-img>
+      </q-card-section>
+      <q-card-section v-if="horizontal" class="col-8 flex items-center">
+        <div class="text-xl md:text-2xl text-left capitalize reward-name-container">
           <div class="space-x-4 flex">
             <div class="name">
               {{ reward.name }}
             </div>
             <carbon-star v-if="reward.claimed" class="claimed text-yellow-400 fill-yellow-400" />
           </div>
-          <div class="text-]">
+          <div class="text-base md:text-xl">
             {{ untilClaimable }}
           </div>
         </div>
-      </q-img>
+      </q-card-section>
     </q-card-section>
-    <q-card-section>
-      <q-btn
-        v-if="props.editable"
-        class="absolute top-0 right-0 -translate-y-8"
-        fab-mini
-        color="primary"
-        :to="{
-          name: 'rewards-rewardid-edit',
-          params: {
-            rewardid: props.reward.id
-          }
-        }"
-      >
-        <carbon-edit />
-      </q-btn>
+    <q-card-section v-if="showProgress">
       <q-btn v-if="claimable(reward)" class="w-full" padding="0px" flat @click="() => claimReward(reward)">
         <RewardProgress
           :value="reward.milestone"
@@ -75,6 +87,21 @@ const untilClaimable = computed(() => {
         :bar-color="reward.claimed ? 'accent' : 'green'"
         :badge-color="reward.claimed ? 'green' : 'accent'"
       />
+    </q-card-section>
+    <q-card-section v-if="editable" class="p-0">
+      <q-btn
+        class="absolute top-0 right-0 -translate-y-8"
+        fab-mini
+        color="primary"
+        :to="{
+          name: 'rewards-rewardid-edit',
+          params: {
+            rewardid: props.reward.id
+          }
+        }"
+      >
+        <carbon-edit />
+      </q-btn>
     </q-card-section>
   </q-card>
 </template>
