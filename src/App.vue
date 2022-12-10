dd182687725f31f096a1b8ac07099b3f3bd98049
aaronmyatt <aaronmyatt@gmail.com>
2022-12-10 20:48:08 +0800

    adds social share images and meta

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,18 @@ useHead({
   title: 'Reward Me',
   meta: [
     { name: 'description', content: 'Rewarding Better Behaviour' },
+    { name: 'og:type', content: 'website' },
+    { name: 'og:title', content: 'Reward Me' },
+    { name: 'og:url', content: 'https://rewardme.me' },
+    { name: 'og:image', content: '/social.jpg' },
+    { name: 'og:site_name', content: 'Reward Me' },
+    { name: 'og:description', content: 'Good deeds, good rewards' },
+    { name: 'twitter:card', content: 'summary' },
+    { name: 'twitter:site', content: '@aaronmyatt' },
+    { name: 'twitter:title', content: 'Reward Me' },
+    { name: 'twitter:url', content: 'https://rewardme.me' },
+    { name: 'twitter:description', content: 'Good deeds, good rewards' },
+    { name: 'twitter:image', content: '/social.jpg' },
   ],
 })
 </script>
